435d225369e06a360fcef6516932cc53e39f1a89
mchesser <michael.chesser@adelaide.edu.au>
2025-06-24 09:50:38 +0930

    Fix example code

--- examples/armv4t/gdb/auxv.rs
+++ examples/armv4t/gdb/auxv.rs
@@ -4,7 +4,12 @@ use gdbstub::target;
 use gdbstub::target::TargetResult;
 
 impl target::ext::auxv::Auxv for Emu {
-    fn get_auxv(&self, offset: u64, length: usize, buf: &mut [u8]) -> TargetResult<usize, Self> {
+    fn get_auxv(
+        &mut self,
+        offset: u64,
+        length: usize,
+        buf: &mut [u8],
+    ) -> TargetResult<usize, Self> {
         let auxv = b"\x00\x00\x00\x00\x00\x00\x00\x00";
         Ok(copy_range_to_buf(auxv, offset, length, buf))
     }
